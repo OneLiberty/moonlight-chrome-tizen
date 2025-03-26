22c8759eb23be6a2645efd916c306dbd48d71260
Dennis <ostermann3k@googlemail.com>
2025-03-26 10:27:34 +0100

    Fix validation of non-pinned trusted TLS certificates
    https://github.com/moonlight-stream/moonlight-chrome/commit/be7b87797bb0b87d6202f19ef497e9a1cefeb4f9

--- wasm/http.cpp
+++ wasm/http.cpp
@@ -106,7 +106,7 @@ void MoonlightInstance::OpenUrl_private(int callbackId, std::string url, std::st
     return;
   }
 
-  err = http_request(url.c_str(), ppk.c_str(), data);
+  err = http_request(url.c_str(), ppk.empty() ? NULL : ppk.c_str(), data);
   if (err) {
     http_free_data(data);
     PostPromiseMessage(callbackId, "reject", std::to_string(err));
