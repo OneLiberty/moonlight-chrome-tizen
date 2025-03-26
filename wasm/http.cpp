d4da83bf9237a637786eb35baa17fa962fb991cb
Dennis <ostermann3k@googlemail.com>
2025-03-26 11:43:14 +0100

    Fix MAIN_THREAD_EM_ASM FS.Mount

--- wasm/http.cpp
+++ wasm/http.cpp
@@ -73,7 +73,7 @@ LoadResult MoonlightInstance::LoadCert(const char* certStr, const char* keyStr) 
 
 MessageResult MoonlightInstance::HttpInit(std::string cert, std::string privateKey, std::string myUniqueId) { 
   // Use Emscripten's virtual file system API to create the directory and preload the CA bundle
-  EM_ASM({
+  MAIN_THREAD_EM_ASM({
       FS.mkdir('/curl');
       FS.mount(FS.filesystems.HTTPFS, { root: '/static/curl' }, '/curl');
   });
@@ -99,9 +99,6 @@ MessageResult MoonlightInstance::HttpInit(std::string cert, std::string privateK
   g_UniqueId = strdup(myUniqueId.c_str());
 
   curl_global_init(CURL_GLOBAL_DEFAULT);
-  
-  // Configure CURL to use the CA bundle - this already done in curl. Maybe not necessary here
-  curl_easy_setopt(curl, CURLOPT_CAINFO, "/curl/ca-bundle.crt");
 
   return MessageResult::Resolve();
 }
