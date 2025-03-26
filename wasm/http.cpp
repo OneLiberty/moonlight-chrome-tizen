a95b887c370c4337d73cb55b59aa2437c4e3e7fd
Dennis <ostermann3k@googlemail.com>
2025-03-26 10:54:40 +0100

    Add a CA bundle for CURL
    https://github.com/dos-ise/moonlight-chrome-tizen_2/commit/232e3c57f3278fab9ff6b0efc2d9d2fbaef3248c#diff-041b28966a7c0cc5f20147b44d9adac3751b1aecb9e7e6ba852a3ca14034c061

--- wasm/http.cpp
+++ wasm/http.cpp
@@ -3,6 +3,7 @@
 #include <errors.h>
 #include <http.h>
 #include <string.h>
+#include <sys/mount.h>
 
 #include <mkcert.h>
 #include <openssl/bio.h>
@@ -72,6 +73,9 @@ LoadResult MoonlightInstance::LoadCert(const char* certStr, const char* keyStr) 
 }
 
 MessageResult MoonlightInstance::HttpInit(std::string cert, std::string privateKey, std::string myUniqueId) {
+  // Mount resource directory where CA bundle resides
+  mount("static/curl", "/curl", "httpfs", 0, "");
+  
   LoadResult res = LoadResult::Success;
   res = LoadCert(cert.c_str(), privateKey.c_str());
   if (res == LoadResult::CertErr) {
